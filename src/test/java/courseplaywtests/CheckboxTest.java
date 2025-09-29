bf941843e878dd1d3b84a86a4c1d0d9bf7c00403
Viktor3RDart <fable198666@mail.ru>
2025-09-29 16:10:12 +0300

    ref Dockerfile v5

--- src/test/java/courseplaywtests/CheckboxTest.java
+++ src/test/java/courseplaywtests/CheckboxTest.java
@@ -20,7 +20,7 @@ public class CheckboxTest {
     @Step("Инициализация браузера и контекста")
     void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         context = browser.newContext();
 
         // Настройка трассировки для скриншотов при ошибках
